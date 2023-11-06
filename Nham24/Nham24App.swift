5c9df43ab6c1a00b2f00543236c805a82a71178d
Chanphirom Sok <65760336+chanphiromsok@users.noreply.github.com>
2023-11-06 09:24:30 +0700

    Initial Commit

---
+++ Nham24/Nham24App.swift
@@ -0,0 +1,17 @@
+//
+//  Nham24App.swift
+//  Nham24
+//
+//  Created by rom on 6/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Nham24App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
